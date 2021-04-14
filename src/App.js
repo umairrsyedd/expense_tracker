d8e0411f732bb90d7a7b0dc9fb3eff00a8bea884
Syed Umair <umair_developer@outlook.com>
2021-04-14 16:17:59 +0530

    Protected Route Implemented for Dashboard & Profile Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import AuthenticationButton from "./Pages/AuthenticationButton";
 import SignUpButton from "./Components/SignUp";
+import Dashboard from "./Pages/Dashboard";
+import Profile from "./Components/Profile";
+import ProtectedRoute from "./Auth/Protected_Route";
 function App() {
   return (
     <>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/auth" component={AuthenticationButton} />
       <Route exact path="/signup" component={SignUpButton} />
+      <ProtectedRoute path="/dashboard" component={Dashboard} />
+      <ProtectedRoute exact path="/profile" component={Profile} />
     </>
   );
 }
---
+++ src/Components/Profile.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+const Profile = () => {
+  const { user } = useAuth0();
+  return <>{JSON.stringify(user, null, 2)}</>;
+};
+
+export default Profile;
