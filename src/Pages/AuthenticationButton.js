dc13ef746012b531a80031fbd63d2d47ed47ce68
Syed Umair <umair_developer@outlook.com>
2021-04-14 16:17:25 +0530

    Authentication Button with Loading State

--- src/Pages/AuthenticationButton.js
+++ src/Pages/AuthenticationButton.js
@@ -1,10 +1,13 @@
 import LoginButton from "../Components/Login";
 import LogoutButton from "../Components/LogOut";
-
+import Loading from "../Components/Loading";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const AuthenticationButton = () => {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return isAuthenticated ? <LogoutButton /> : <LoginButton />;
 };
