869112b198f89fdadb5e4237a7ece92ea61bcd43
Syed Umair <umair_developer@outlook.com>
2021-04-18 13:10:10 +0530

    Added Login | Signup on Home Page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,7 +1,17 @@
-export default function HomePage() {
-  return (
-    <>
-      <div>Home Page</div>
-    </>
-  );
+import React from "react";
+import { Link } from "react-router-dom";
+export default class HomePage extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>Expense Tracker Built Using React, Express, MongoDB</h2>
+        <Link to="/auth">
+          <button> Log In </button>
+        </Link>
+        <Link to="/signup">
+          <button> SignUp </button>
+        </Link>
+      </>
+    );
+  }
 }
