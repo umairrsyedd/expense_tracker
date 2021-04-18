393551cd341abcd1f0f7035634964332f38e56fc
Syed Umair <umair_developer@outlook.com>
2021-04-18 21:53:19 +0530

    Added Cards with Loading State on Dashboard

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,6 +1,59 @@
 import React from "react";
 import { Layout } from "antd";
+import { Skeleton, Switch, Card } from "antd";
 const { Content } = Layout;
-export default function Dashboard() {
-  return <Content style={{ margin: "0 16px" }}>Welcome to Dashboard</Content>;
+const { Meta } = Card;
+
+export default class Dashboard extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  onChange = (checked) => {
+    this.setState({ loading: !checked });
+  };
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <>
+        <Content style={{ margin: "0 16px", display: "flex" }}>
+          <Switch checked={!loading} onChange={this.onChange} />
+          <Card
+            style={{ width: 450, marginTop: 16, height: 250 }}
+            loading={loading}
+          >
+            <Skeleton loading={loading} avatar active>
+              <Meta title="Income By Category" description="Last 5 Expenses" />
+            </Skeleton>
+          </Card>
+          <Card
+            style={{ width: 450, marginTop: 16, height: 250 }}
+            loading={loading}
+          >
+            <Skeleton loading={loading} avatar active>
+              <Meta title="Expense By Category" description="Last 5 Expenses" />
+            </Skeleton>
+          </Card>
+          <Card
+            style={{ width: 350, marginTop: 16, height: 200 }}
+            loading={loading}
+          >
+            <Skeleton loading={loading} avatar active>
+              <Meta title="Recent Incomes" description="Last 5 Expenses" />
+            </Skeleton>
+          </Card>
+          <Card
+            style={{ width: 350, marginTop: 16, height: 200 }}
+            loading={loading}
+          >
+            <Skeleton loading={loading} avatar active>
+              <Meta title="Recent Expenses" description="Last 5 Expenses" />
+            </Skeleton>
+          </Card>
+        </Content>
+      </>
+    );
+  }
 }
