5b6753e2e877dd19ee7527d18b2fd701b0d503bd
Syed Umair <umair_developer@outlook.com>
2021-05-09 01:29:58 +0530

    Added Login & Logout Button Styling for Home Page & Dashboard

--- src/Components/LogOut.js
+++ src/Components/LogOut.js
@@ -1,10 +1,20 @@
 import { useAuth0 } from "@auth0/auth0-react";
-const LogoutButton = () => {
+const LogOut = () => {
   const { logout } = useAuth0();
+  const buttonStyle = {
+    border: "none",
+    textAlign: "center",
+    fontSize: "1rem",
+    backgroundColor: "#278CF3",
+    color: "#FFFFFF",
+    cursor: "pointer",
+    // marginLeft: "auto",
+    // marginRight: "1vw",
+    padding: "0px 10px 0px 10px",
+  };
   return (
     <button
-      variant="contained"
-      color="primary"
+      style={buttonStyle}
       onClick={() =>
         logout({
           returnTo: window.location.origin,
@@ -16,4 +26,4 @@ const LogoutButton = () => {
   );
 };
 
-export default LogoutButton;
+export default LogOut;
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,14 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import "../Styles/homepageStyles.css";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
     <>
-      <button
-        variant="contained"
-        color="primary"
-        onClick={() => loginWithRedirect()}
-      >
-        Log In
+      <button className="heroButton" onClick={() => loginWithRedirect()}>
+        Log In | Register
       </button>
     </>
   );
